ec2cdefd8df06f912c76b4299c7be6aeaed53d3a
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2020-12-16 14:49:18 -0600

    react serves static files from public

--- inc/src/components/Musician.js
+++ inc/src/components/Musician.js
@@ -4,11 +4,11 @@ import Score from "../utils/score"
 
 export default function Musician({ name, synth, transpose }) {
     const [isPlaying, setIsPlaying] = useState(false)
-    const [onPhrase, setOnPhrase] = useState(0)
+    const [onPhrase, setOnPhrase] = useState(1) //1dexed for human readability
     //b/c I memoize the musician in order to preserve its identity/integrity, the render phase never reconsiders it's changed property values ergo useState
     const musician = useMemo(() => {
         return new AbstractMusician(name, synth, new Score(transpose))
-    }, [name, synth, Score])
+    }, [name, synth, transpose])
 
     const timeOuts = () => {
         const tOs = [10, 100, 1000]
@@ -20,13 +20,14 @@ export default function Musician({ name, synth, transpose }) {
     const wrapInTimeOut = function (func, tOut) {
         return setTimeout(() => func(tOut), tOut)
     }
-    //show phraseNum
+    //show phraseNum=> not getting 2nd image but others
     return (<div style={{ width: '20%', float: 'left' }}>
-        <div onClick={() => timeOuts()}>{musician.name || "nobody"} {musician.phrase}</div>
+        <div onClick={() => timeOuts()}>{musician.name || "nobody"} {onPhrase}</div>
         {isPlaying ?
             <p onClick={() => { musician.doNote(true); setOnPhrase(x => x + 1) }}>prod</p> :
             <p onClick={() => { musician.doNote(); setIsPlaying(true) }}>play</p>
         }
+        <img src={"/static/ph" + onPhrase + ".png"}></img>
     </div>
     )
 
