eb020920b976e4c0fdefd5296cc373381d284cf8
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-02-24 21:54:46 -0600

    methods on prototypes are more effecient

--- phaserVersion/public/AbstractMusician.js
+++ phaserVersion/public/AbstractMusician.js
@@ -1,4 +1,5 @@
 let Gain = Tone.Gain
+const bpmToMilliseconds = beatsPerMinute => beatsPerMinute
 class Musician {
     constructor(name, synth, score) {
         this.name = name
@@ -8,42 +9,46 @@ class Musician {
         this.gainNode = new Gain(this.volume).toDestination();
         this.instrument = synth;
         this.instrument.connect(this.gainNode)
-        this.scale = .082;//tempo in seconds
+        this.scale = .082;// tempo of 32nd Note in seconds
         this.TO = null;
         this.ready = true;
         this.readyTO = null;
 
     }
-    //hasAx = function () { return this.instrument !== undefined }
-    waitHalfASec = function () {
-        this.ready = false
-        this.readyTO = setTimeout(() => {
-            this.ready = true
-        }, 500)
-    }
-    doNote = function (quittingSoon) {
-        //might be nice to make quittingSoon finish phrase
-        if (!quittingSoon) {
-            clearTimeout(this.TO)
-        } else { this.waitHalfASec() }
+} 
 
-        const genRes = this.score.phrases[this.phrase].gen.next(quittingSoon)
-        if (!genRes.done) {
-            const { pitch, duration } = genRes.value
-            this.instrument.triggerAttackRelease(pitch, duration * this.scale, now())
-            const scaledToMs = duration * this.scale * 1000
-            this.TO = setTimeout(
-                (helper, willQuit) => { //params
-                    helper.apply(this, [willQuit])
-                },
-                scaledToMs,
-                this.doNote, quittingSoon); //args
-        } else {
-            console.log(`${this.name} is moving on from ${this.phrase}`)
-            this.phrase++
-        }
+Musician.prototype.waitHalfASec = function () {
+    this.ready = false
+    this.readyTO = setTimeout(() => {
+        this.ready = true
+    }, 500)
+    }
+
+Musician.prototype.doNote = function (quittingSoon) {
+    //might be nice to make quittingSoon finish phrase
+    if (!quittingSoon) {
+        clearTimeout(this.TO)
+    } else {
+        this.waitHalfASec()
+    }
+
+    const genRes = this.score.phrases[this.phrase].gen.next(quittingSoon)
+    if (!genRes.done) {
+        const {pitch, duration    } = genRes.value
+        this.instrument.triggerAttackRelease(pitch, duration * this.scale, now())
+        const scaledToMs = duration * this.scale * 1000
+        this.TO = setTimeout(
+        (helper, willQuit) => { //params
+        helper.apply(this, [willQuit])
+            },
+        scaledToMs,
+        this.doNote, quittingSoon); //args
+    } else {
+        console.log(`${this.name} is moving on from ${this.phrase}`)
+        this.phrase++
     }
 }
-    Musician.prototype.setVolume = function (gain) {
+
+Musician.prototype.setVolume = function (gain) {
     this.gainNode.gain.rampTo(gain, .1)
 }
